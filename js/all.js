5b802904a8e2b595876e21d3ed21df3015e69322
Hammad Ali <hammad19ali@gmail.com>
2023-01-03 21:36:16 +0500

    final files

--- js/all.js
+++ js/all.js
@@ -1,7 +1,9 @@
 // ******************** place order ********************* //
 let orderItems;
 let cartItems;
+let completedOrders
 $(document).ready(function () {
+    // cart items
     if (localStorage.getItem("lasthourcart") != null) {
         cartItems = JSON.parse(localStorage.getItem("lasthourcart"))
         appendCart()
@@ -9,11 +11,19 @@ $(document).ready(function () {
     else {
         cartItems = []
     }
+    // order items
     if (localStorage.getItem("lasthourorder") != null) {
         orderItems = JSON.parse(localStorage.getItem("lasthourorder"))
     }
     else {
         orderItems = []
+    }
+    // completed orders 
+    if (localStorage.getItem("lasthourcompleted") != null) {
+        completedOrders = JSON.parse(localStorage.getItem("lasthourcompleted"))
+    }
+    else {
+        completedOrders = []
     }
 })
 $('.side_collapse_parent').click(function () {
@@ -46,8 +56,11 @@ $("body").click(function (e) {
 })
 
 function addtocart() {
-    let tempItem
-    tempItem = {
+    let tempData
+    if ($(".addtocart-form textarea[name='additional-info']").val() == "") {
+        $(".addtocart-form textarea[name='additional-info']").val("(no additionl info given)")
+    }
+    tempData = {
         id: Date.now(),
         additionalInfo: $(".addtocart-form textarea[name='additional-info']").val(),
         quantity: $(".addtocart-form input[name='quantity']").val(),
@@ -56,7 +69,7 @@ function addtocart() {
     }
     $('.side_collapse_parent').removeClass("active")
     $(".side_collapse").remove()
-    cartItems.push(tempItem)
+    cartItems.push(tempData)
     localStorage.setItem("lasthourcart", JSON.stringify(cartItems))
     createToast("1 item has been added to cart!")
     appendCart()
@@ -121,16 +134,116 @@ function removeCartItem(btn) {
     let targetIndex = cartItems.indexOf(tempItem)
     cartItems.splice(targetIndex, 1)
     localStorage.setItem("lasthourcart", JSON.stringify(cartItems))
+    createToast(`${tempItem.quantity}x ${tempItem.item} has been removed!`)
     appendCart()
 }
 
-$(".place-order").click(async function(){
-    await orderItems.push(cartItems)
+$(".place-order").click(function () {
+    let orderno = localStorage.getItem("lasthourorderno") ? localStorage.getItem("lasthourorderno") : 201
+    let tempItem = {
+        id: orderno,
+        items: cartItems
+    }
+    localStorage.setItem("lasthourorderno", Number(orderno) + 1)
+    orderItems.push(tempItem)
+    localStorage.setItem("lasthourorder", JSON.stringify(orderItems))
     localStorage.removeItem("lasthourcart")
+    createToast("Order Placed Successfully!")
+    $(".close-canvas").click()
     cartItems = []
     appendCart();
-    window.location.href = "currentOrders.html"
 })
 
 
-// ******************** place order ********************* //
+// ******************** Order Info ********************* //
+function renderOrderData(order, elem) {
+    let tempData = "";
+    $(elem).html("")
+    // order.forEach(curOrder => function(){
+    for (let i = 0; i < order.length; i++) {
+        if (elem == "#queue-order" && i == 0) {
+            continue;
+        }
+        tempData += `
+        <div class="your_account_card">
+            <div class="your_account_card_inner">
+                <div class="your_account_card_inner_content">
+                    <div class="your_note">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <div class="d-flex align-items-center">
+                                <img src="images/checklist.png" alt="">
+                                <h6>Order Summary</h6>
+                            </div>
+                            <span>Order number: ${order[i].id}</span>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="order_summary">
+                    <ul class="p-0 m-0">`
+        order[i].items.map(curItem => {
+            tempData += `<li>
+                            <div><strong>x${curItem.quantity}</strong> ${curItem.item}</div><strong>${curItem.price} PKR</strong>
+                        </li>`
+        })
+        tempData += `</ul>
+                </div>
+            </div>
+        </div>
+            `
+    }
+    if(tempData != ""){
+        $(elem).html(tempData)
+    }
+    else{
+        if(elem == "#queue-order"){
+            $(elem).html(`<p class="no-data-text">None of the order is Pending!</p>`)
+        }
+        else{
+            $(elem).html(`<p class="no-data-text">None of the order is Completed!</p>`)
+        }
+    }
+}
+function renderActiveOrder() {
+    let tempItem = "";
+    if (orderItems.length > 0) {
+        $("#active-order-no").html("#" + orderItems[0].id)
+        orderItems[0].items.map(curItem => {
+            tempItem += `<li>
+                <h6><span>${curItem.item}</span><span>${curItem.quantity}</span></h6>
+                <p>${curItem.additionalInfo}</p>
+            </li>`
+        })
+        $("#active-order-items").html(tempItem)
+        let progress = $(".order-processing-card .progress")
+        let progressBarWidth = 0;
+        $(progress).removeClass("d-none")
+        let proressInterval = setInterval(() => {
+            if (progressBarWidth < 100) {
+                progressBarWidth += 1
+                $(progress).children().css("width", `${progressBarWidth}%`)
+            }
+        }, 200);
+        setTimeout(() => {
+            $("#active-order-no").html("")
+            completedOrders.push(orderItems[0]);
+            createToast(`order # ${orderItems[0].id} has been ready`)
+            orderItems.splice(0, 1)
+            localStorage.setItem("lasthourcompleted", JSON.stringify(completedOrders))
+            localStorage.setItem("lasthourorder", JSON.stringify(orderItems))
+            clearInterval(proressInterval);
+            renderOrderData(orderItems, "#queue-order")
+            renderOrderData(completedOrders, "#order-completed")
+            renderActiveOrder()
+        }, 20000);
+    }
+    else{
+        $(".order-processing-card .progress").addClass("d-none")
+        $("#active-order-items").html(`<p class="no-data-text">None of the order is in process!</p>`)
+    }
+}
+$(document).ready(function () {
+    renderOrderData(orderItems, "#queue-order")
+    renderOrderData(completedOrders, "#order-completed")
+    renderActiveOrder()
+})
